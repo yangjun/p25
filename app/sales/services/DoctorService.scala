8b9eca55d0f6376a98db2aa8463df56f61b97dcd
yangjun <yangjun.chengdu@gmail>
2016-05-06 22:04:38 +0800

    医生服务

--- app/sales/services/DoctorService.scala
+++ app/sales/services/DoctorService.scala
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory
 import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.play.json.collection.JSONCollection
-import sales.models.{AddDoctor, CreateHospital, Error, Hospital}
+import sales.models._
 import utils.Utils
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -47,7 +47,9 @@ class DoctorService @Inject()(
     * <pre>
     * 1. 检查医院标识是否存在
     * 2. 如果填写了电话号码，根据电话号码检查是否是已经存在的用户
-    *
+    * 2.1 如果用户存在，取用户ID
+    * 2.2 如果用户不存在，添加用户
+    * 3. 根据用户ID构造医生信息，添加医生
     * </pre>
     *
     * @param addDoctor
@@ -58,7 +60,7 @@ class DoctorService @Inject()(
             (implicit ec: ExecutionContext): Future[String] = {
     val id = Utils.nextId
     val hospitalId = addDoctor.hospital
-    hospitalId match {
+    val userId = hospitalId match {
       case Some(hid) => {
         // 查询医院
         pk(hid).flatMap(h => {
@@ -114,6 +116,21 @@ class DoctorService @Inject()(
         throw new RuntimeException("需要指定医院标识")
       }
     }
+    // 创建
+    userId flatMap (u => {
+      val doctor = Doctor(id,
+        Some(u),
+        addDoctor.name,
+        addDoctor.job,
+        hospital = addDoctor.hospital.getOrElse("")
+      )
+      doctorCollection.flatMap(_.insert(doctor)) map {
+        case le if le.ok => id
+        case le =>
+          logger.error(le.message)
+          throw new RuntimeException("新建失败")
+      }
+    })
   }
 
 
